a9d91e17fce686b68dc1cc0cc1a73e8348ab628b
František Bráblík <frantisek.brablik@gmail.com>
2024-12-13 17:08:56 +0100

    Test: Add more complex test case

--- tests.py
+++ tests.py
@@ -94,6 +94,59 @@ class TestGameplay(unittest.TestCase):
         game = Game(board=b)
         self.assertFalse(all(map(all, game.available_boards())))
 
+    def test_elaborate_game(self):
+        turns: list[Tuple[str, str, int, int]] = [
+            ('X', 'board', 1, 1),
+            ('X', 'put',   1, 1),
+            ('O', 'put',   1, 0),
+            ('X', 'put',   1, 1),
+            ('O', 'put',   2, 2),
+            ('X', 'put',   1, 1),
+            ('O', 'put',   2, 0),
+            ('X', 'put',   1, 1),
+            ('O', 'put',   1, 2),
+            ('X', 'put',   0, 0),
+            ('O', 'put',   2, 1),
+            ('X', 'put',   2, 2),
+            ('O', 'put',   2, 1),
+            ('X', 'put',   1, 1),
+            ('O', 'put',   0, 0),
+            ('X', 'put',   1, 1),
+            ('O', 'board', 2, 1),
+            ('O', 'put',   0, 2),
+            ('X', 'put',   1, 1),
+            ('O', 'board', 2, 1),
+            ('O', 'put',   2, 1),
+            ('X', 'put',   0, 0),
+            ('O', 'put',   0, 2),
+            ('X', 'put',   0, 0),
+            ('O', 'put',   2, 0),
+            ('X', 'put',   2, 2),
+            ('O', 'put',   2, 0),
+            ('X', 'put',   0, 0),
+            ('O', 'put',   2, 2),
+            ('X', 'put',   2, 2),
+            ('O', 'put',   0, 1),
+            ('X', 'put',   1, 1),
+            ('O', 'board', 1, 2),
+            ('O', 'put',   2, 2),
+            ('X', 'put',   0, 0),
+            ('X', 'won',   0, 0),
+        ]
+        game = Game(first=Box.X)
+        for symbol, cmd, x, y in turns:
+            if cmd != 'won':
+                self.assertEqual(game.playing().name, symbol)
+            if cmd == 'board':
+                self.assertTrue(game.should_select())
+                game.select((x, y))
+            elif cmd == 'put':
+                self.assertFalse(game.should_select())
+                game.place((x, y))
+                game.next_round()
+            elif cmd == 'won':
+                expected = State.X_WON if symbol == 'X' else State.O_WON
+                self.assertEqual(game.state(), expected)
 
 if __name__ == "__main__":
     unittest.main()
